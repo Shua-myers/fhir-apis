8ca585039d55687630de89b4483dd515f97f09c1
Joshua Myers <joshua.a.myers@gmail.com>
2024-01-19 13:17:32 -0500

    Patient CRUS routes stubbed out

--- python/fastapi-fhir/main.py
+++ python/fastapi-fhir/main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.post("/Patient/")
+async def create_patient():
+    return {"the created resource"}
+
 @app.get("/Patient/{id}")
 async def read_patient(id: int):
-    return {"patient": id}
+    return {f"read resource for {id}"}
+
+@app.put("/Patient/{id}")
+async def update_patient(id: int):
+    return {"the updated resource for {id}" }
+
+@app.get("/Patient/")
+async def search_patient():
+    return {"a patient search bundle"}
