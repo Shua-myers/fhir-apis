c464562a0ca97b0888276da61ac758513ace1a04
Joshua Myers <joshua.a.myers@gmail.com>
2024-01-19 13:58:20 -0500

    create_patient implements Patient model for input and output

--- python/fastapi-fhir/main.py
+++ python/fastapi-fhir/main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+# fhir.resources could do this for us
+class Patient(BaseModel):
+    id: str
+    active: bool = True
+    name: dict
+    telecom: dict | None = None
+    gender: str
+    birthDate: str
 
 app = FastAPI()
 
 
 @app.post("/Patient/")
-async def create_patient():
-    return {"the created resource"}
+async def create_patient(patient: Patient):
+    # TODO: save to database
+    return patient
 
 @app.get("/Patient/{id}")
 async def read_patient(id: int):
@@ -16,5 +28,5 @@ async def update_patient(id: int):
     return {"the updated resource for {id}" }
 
 @app.get("/Patient/")
-async def search_patient():
+async def search_patient(_id: int | None = None, patient: int | None = None, name: str | None = None):
     return {"a patient search bundle"}
